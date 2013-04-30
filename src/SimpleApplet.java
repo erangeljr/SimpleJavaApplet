73dcaef0626e71ca0161f1fb4a199853439682fd
Eddie Rangel <hacker@eddierangel.com>
2013-04-30 10:33:39 -0700

    Created SimpleApplet Class
    Class extends JApplet and implements ActionListener
    Added actionPerformed method stub.

---
+++ src/SimpleApplet.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+import java.awt.event.*;
+
+import javax.swing.*;
+import java.util.*;
+
+
+
+public class SimpleApplet extends JApplet implements ActionListener{
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
