fee35d2054ac94388668f1dc6c6df9ff3f760c8b
Eddie Rangel <hacker@eddierangel.com>
2013-04-30 10:47:58 -0700

    Imported SortCons
    Pulled in SortCons class

--- src/SimpleApplet.java
+++ src/SimpleApplet.java
@@ -1,12 +1,18 @@
 import java.awt.*;
 import java.awt.event.*;
-
 import javax.swing.*;
 import java.util.*;
 
 
 
 public class SimpleApplet extends JApplet implements ActionListener{
+	
+	
+	JButton btnSort[] = { new JButton("Generate"), 
+				new JButton("Sort"), new JButton("Shuffle") };
+    JPanel  panSouth = new JPanel();
+    static  Canvas  drawingBoard = new Canvas();
+    int	   data[] = new int [SortCons.ARRAY_SIZE];
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
