24eb7d8fd2b61bd3c6128203606f18240ca4cab7
Eddie Rangel <hacker@eddierangel.com>
2013-04-30 10:56:20 -0700

    Added Components
    Adding Content Pane with Buttons

--- src/SimpleApplet.java
+++ src/SimpleApplet.java
@@ -14,10 +14,49 @@ public class SimpleApplet extends JApplet implements ActionListener{
     static  Canvas  drawingBoard = new Canvas();
     int	   data[] = new int [SortCons.ARRAY_SIZE];
 
+    public SimpleApplet() {
+    	//Default Constructor
+    }
+    
+    //Initialize container and components
+    public void init() {
+    	Container c = getContentPane();
+    	c.setSize(650, 500);
+
+    	c.setLayout(new BorderLayout() );
+    	for ( int i = 0; i < btnSort.length; i ++ ) {
+    		btnSort[i].addActionListener(this);
+    		if ( i > 0 )  btnSort[i].setEnabled(false);
+    		panSouth.add(btnSort[i]);
+    	}
+    	c.add(panSouth, BorderLayout.SOUTH);
+    	c.add(drawingBoard, BorderLayout.CENTER);
+    	drawingBoard.setBackground(Color.black);
+    }
+    
+    public void start() {
+    }
+    
+    
 	@Override
-	public void actionPerformed(ActionEvent arg0) {
+	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
+		int k = -1;
+		Object o = e.getSource();
+		for ( int i = 0; i < btnSort.length; i ++ ) {
+			if ( o == btnSort[i] ) { k = i; break; }
+		}
 		
 	}
+	
+	 public static void main( String argv[] ) {
+			JFrame fmObj = new JFrame("Simple Java Applet");
+			fmObj.addWindowListener( new  WindowAdapter() {
+			public void windowClosing( WindowEvent e ) { System.exit(0); } } );
+			fmObj.setSize(600, 500);			
+			fmObj.getContentPane();
+			fmObj.setVisible(true);
+			
+		   }
 
 }
